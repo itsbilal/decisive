8bbc0128a4bc4ca519efd515fd485e33052c74a9
Bilal Akhtar <me@itsbilal.com>
2016-03-27 16:42:19 -0400

    Some initial sequelize stuff

---
+++ migrations/20160327163946-create-poll.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Polls', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      creator: {
+        type: Sequelize.STRING
+      },
+      open: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Polls');
+  }
+};
